f2acfc3defb01cc73e1288c588b4046ab1e33c76
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2024-01-02 17:14:19 +0600

    make task left count dynamic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
+import { useSelector } from "react-redux";
+
 export default function Footer() {
+    const todos = useSelector(state => state)
+    const pendingTodos = todos.filter(todo => todo.status === 'pending')
     return (
         <div className="mt-4 flex justify-between text-xs text-gray-500">
-            <p>2 tasks left</p>
+            <p>{pendingTodos?.length} tasks left</p>
             <ul className="flex space-x-1 items-center text-xs">
                 <li className="cursor-pointer font-bold">All</li>
                 <li>|</li>
