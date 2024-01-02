f48800d5edd70262bf5d94cf71abeeaa77beb45c
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2024-01-02 13:44:27 +0600

    add action type for complete a todo

--- src/redux/todo/actionTypes.js
+++ src/redux/todo/actionTypes.js
@@ -1,4 +1,5 @@
 export const ADD_TODO = "todo/add_todo"
 export const DELETE_TODO = "todo/delete-todo"
+export const COMPLETE_A_TODO = "todo/complete_a_todo"
 export const COMPLETE_ALL_TODO = "todo/complete_all_todo"
 export const CLEAR_COMPLETED_TODO = "todo/clear_completed_todo"
