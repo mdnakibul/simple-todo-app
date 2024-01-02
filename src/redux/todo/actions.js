ad524c5570f027844aca579a23ea3bc922adee9a
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2024-01-02 13:44:13 +0600

    add action creator for completing a todo

--- src/redux/todo/actions.js
+++ src/redux/todo/actions.js
@@ -1,4 +1,4 @@
-import { ADD_TODO, CLEAR_COMPLETED_TODO, COMPLETE_ALL_TODO, DELETE_TODO } from "./actionTypes"
+import { ADD_TODO, CLEAR_COMPLETED_TODO, COMPLETE_ALL_TODO, COMPLETE_A_TODO, DELETE_TODO } from "./actionTypes"
 
 export const addTodo = (payload) => {
     return {
@@ -9,6 +9,12 @@ export const addTodo = (payload) => {
 export const deleteTodo = (payload) => {
     return {
         type: DELETE_TODO,
+        payload
+    }
+}
+export const completeATodo = (payload) => {
+    return {
+        type: COMPLETE_A_TODO,
         payload
     }
 }
