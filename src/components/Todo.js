66483ae0f3fdb02a86c049381aaa7cbb9ca142e0
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2024-01-02 13:44:00 +0600

    handle complete single todo case

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,6 @@
 import { useDispatch } from "react-redux";
 import cancelImage from "../assets/images/cancel.png";
-import { deleteTodo } from "../redux/todo/actions";
+import { completeATodo, deleteTodo } from "../redux/todo/actions";
 
 export default function Todo({ todo }) {
     const dispatch = useDispatch()
@@ -8,20 +8,30 @@ export default function Todo({ todo }) {
     const handleDeleteTodo = () => {
         dispatch(deleteTodo({ todoId }))
     }
+
+    const handleCompleteTodo = (e) => {
+        const isChecked = e.target.value
+        if (isChecked) {
+            dispatch(completeATodo({ todoId }))
+        }
+    }
     return (
         <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
             <div className="rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 border-green-500 focus-within:border-green-500">
                 <input
                     type="checkbox"
-                    className="opacity-0 absolute rounded-full"
+                    className="appearance-none opacity-0 absolute rounded-full h-5 w-5"
+                    id="todoCheckbox"
+                    onChange={handleCompleteTodo}
                 />
                 <svg
-                    className="hidden fill-current w-3 h-3 text-green-500 pointer-events-none"
+                    className={`${status !== 'completed' && 'hidden'} fill-current w-3 h-3 text-green-500 pointer-events-none`}
                     viewBox="0 0 20 20"
                 >
                     <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
                 </svg>
             </div>
+
 
             <div className={`select-none flex-1 ${status === 'completed' && 'line-through'}`}>
                 {text}
