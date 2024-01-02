aaf9710eb0c939dc60b8fe6100d91158e893631a
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2024-01-02 13:25:20 +0600

    fix todo id variable name

--- src/redux/todo/todoReducer.js
+++ src/redux/todo/todoReducer.js
@@ -5,11 +5,11 @@ const initialTodoState = []
 const todoReducer = (state = initialTodoState, action) => {
     switch (action.type) {
         case ADD_TODO:
-            const lastTodoId = state[state.length - 1]?.id || 1
+            const lastTodoId = state[state.length - 1]?.todoId || 0
             return [...state, { ...action.payload, todoId: lastTodoId + 1 }]
 
         case DELETE_TODO:
-            return state.filter(todo => todo.id !== action.payload.todoId)
+            return state.filter(todo => todo.todoId !== action.payload.todoId)
 
         case COMPLETE_ALL_TODO:
             return [...state].map(todo => todo.status = "completed")
