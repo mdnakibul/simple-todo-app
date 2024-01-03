e8c2737a02febc73e8eecb091bc07a9f0da0271d
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2024-01-03 16:24:03 +0600

    implement filter in rendering todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,11 +3,27 @@ import Todo from "./Todo";
 
 export default function TodoList() {
     const todos = useSelector(state => state.todos)
-    console.log('todos', todos)
+    const filters = useSelector(state => state.filters)
     return (
         <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
             {
-                todos?.map(todo => <Todo todo={todo} key={todo.todoId} />)
+                todos?.filter(singleTodo => {
+                    switch (filters.status) {
+                        case 'Complete':
+                            return singleTodo.status === 'completed'
+
+                        case 'Incomplete':
+                            return singleTodo.status === 'pending'
+
+                        default:
+                            return true;
+                    }
+                })?.filter(singleTodo => {
+                    if (filters.color.length > 0) {
+                        return filters.color.includes(singleTodo?.color)
+                    }
+                    return true;
+                }).map(todo => <Todo todo={todo} key={todo.todoId} />)
             }
         </div>
     );
